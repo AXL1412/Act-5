941216efb801af5cd8ccaa225a15641d257bdca9
AXL1412 <al03109401@tecmilenio.mx>
2025-09-20 18:58:39 -0600

    ACT5 - Actualización AgendaPickers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.poutividad.act5"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
